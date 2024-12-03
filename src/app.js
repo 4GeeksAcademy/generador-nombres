e9d16bf60c0532f355d0e3055b81145e5e7cc79a
PauVentura30 <pauventuraa@gmail.com>
2024-12-03 14:25:50 +0000

    fin

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hola Rigo!");
 };
+
+let pronoun = ['el', 'nuestro', 'su'];
+  let adj = ['buen', 'gran', 'increible' ];
+  let noun = ['perro', 'gato', 'loro', 'hornitorrinco', 'pato'];
+  
+  // generating the domains
+  for(let i = 0; i < pronoun.length; i++) {
+    for(let j = 0; j < adj.length; j++) {
+      for(let k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k]+'.com'); 
+      }
+    }
+  }
